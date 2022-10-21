9e7bc2d2c3f6a8463c2f3711c8a4eb764c33ab56
Julian Rodriguez <mjulianrodriguez@hotmail.com>
2022-10-21 12:44:00 -0500

    Initial commit

---
+++ src/com/julianrodriguez/Examples.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.julianrodriguez;
+
+/**
+ * @author mr185342
+ *
+ */
+public class Examples {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
